fc0b83bd8842be4a0c8c085606a918d2e8a97cc9
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2025-12-29 12:56:59 +0530

    slicing

--- tuples.py
+++ tuples.py
@@ -1,9 +1,25 @@
 
 # must has atleat 1 comma
-t1=(1,5,8,9)
-print(t1.count(10))
+# t1=(1,5,8,9)
+# print(t1.count(10))
+# print(max(t1))
+# print(min(t1))
+
 
 
 
 # Methods 
 # count , min, max, index 
+
+# Slicing 
+#1 Positive indexed slicing -> index number is positive x,y-> positive
+# t1[starting_index(x),ending index(y) , number of skips(z)]
+# for negative switching  x,y->Negative
+
+#3 combination slicing use both negative and positive values 
+
+
+t1=(1,5,8,9,6,3)
+e1=t1[::-1]
+print(e1)
+
