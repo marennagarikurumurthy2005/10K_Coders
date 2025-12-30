320fc6685ec7ab9d9bf4329872bd0f63ec12c08a
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2025-12-30 11:39:43 +0530

    problem count on vowels

---
+++ Solved_problems/2nd10.py
@@ -0,0 +1,15 @@
+# #1 
+# def countVowels(s):
+#     """
+#     s: str
+#     Returns: int
+#     """
+#     # 1TODO: Implement your solution here
+#     vowels=["a","A","e","E","i","I","o","O","u","U"]
+#     count=0
+#     n=len(s)
+#     for i in range(n):
+#         if s[i]!=" " and s[i] in vowels:
+#             count+=1
+
+#     return count
