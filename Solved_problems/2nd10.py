929012a0d8673fbb02ac52088e24b23224c405a8
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2025-12-30 19:38:02 +0530

    print max len word in sentence

--- Solved_problems/2nd10.py
+++ Solved_problems/2nd10.py
@@ -13,3 +13,38 @@
 #             count+=1
 
 #     return count
+
+
+input= "I enjoy coding"
+words=input.split()
+
+dici={}
+for i in words:
+    if i in dici:
+        pass
+    else:
+        dici[i]=len(i)
+text=["0"]
+maxi=0
+for i in dici:
+    if dici[i]>maxi:
+        maxi=dici[i]
+        text[0]=i
+print(text[0])
+
+
+
+
+
+# def longestWord(sentence):
+#     """
+#     sentence: str
+#     Returns: str
+#     """
+#     # 1TODO: Implement your solution here
+#     longest=""
+#     words=sentence.split()
+#     for word in words:
+#         if len(word)>len(longest):
+#             longest=word
+#     return longest
