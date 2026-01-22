d5582de6bafdf71c7398fb2fa521294a23ef5a6b
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2026-01-22 11:42:27 +0530

    conditional statements problems

---
+++ conditional_statements.py
@@ -0,0 +1,99 @@
+# num=10
+# if num%2==0 and num%3==0 and num%6==0:
+#     print(num,"is divisible by 2,3 and 6")
+# else:
+#     print("not divisible by 2,3 and 6")
+
+
+# maths=45
+# science=150
+# social=48
+# count=0
+# passmarks=35
+# n=2
+# if maths<=passmarks and science<=passmarks and social<=passmarks:
+#     print("student passed ")
+# else:
+#     print("student failed ")
+
+# if maths>=passmarks:
+#     count+=1
+# if science>=passmarks:
+#     count+=1
+# if social>=passmarks:
+#     count+=1
+# print(count)
+# if count>=n : # here n is numbers sub should be passed to be the result is positive
+#     print("passed")
+# else:
+#     print("Failed")
+
+# maths=45
+# science=150
+# social=48
+# # biggest among 3 numbers note: for the smallest number the > symbol will be replaced with <
+# if maths>=science and maths >=social:
+#     print("maths is greater")
+# elif science >=maths and science >=social:
+#     print("science is greater")
+# else:
+#     print("social is greater")
+
+
+# perfect square
+# import math
+
+# # num=42
+# # ext=int(math.sqrt(num))
+# # if ext*ext==num:
+# #     print("perfect square")
+# # else:
+# #     print("not a perfect square")
+
+# # number of cars needed
+# mem=17
+# # cars=math.ceil(mem/5)
+# # print(cars)
+# cars=mem//5
+# if mem%5!=0:
+#     print(cars+1)
+# else:
+#     print(cars)
+
+
+# second largest
+# num1=10
+# num2=2
+# num3=3
+# sum=num1+num2+num3
+
+# maxi=max(num1,num2,num3)
+# mini=min(num1,num2,num3)
+# second=sum-(maxi+mini)
+# print(second)
+
+# num1=10
+# num2=20
+# num3=33
+# if num1>num2:
+#     if num2>num3:
+#         print(num2)
+#     else:
+#         print(num3)
+# else:
+#     if num1>num3:
+#         print(num1)
+#     else:
+#         print(num3)
+
+
+
+
+
+
+
+
+
+
+
+
