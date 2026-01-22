d0ac57a6614f7a8141ac9356e6ac19b2c4f7e656
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2026-01-22 16:00:06 +0530

    conditional statemnts

--- conditional_statements.py
+++ conditional_statements.py
@@ -62,15 +62,15 @@
 
 
 # second largest
-# num1=10
-# num2=2
-# num3=3
-# sum=num1+num2+num3
+num1=10
+num2=2
+num3=3
+sum=num1+num2+num3
 
-# maxi=max(num1,num2,num3)
-# mini=min(num1,num2,num3)
-# second=sum-(maxi+mini)
-# print(second)
+maxi=max(num1,num2,num3)
+mini=min(num1,num2,num3)
+second=sum-(maxi+mini)
+print(second)
 
 # num1=10
 # num2=20
