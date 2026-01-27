b22d83e3c4029047133c8282c135a700b0daf9ca
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2026-01-27 10:50:52 +0530

    day2-1 loops

--- loops.py
+++ loops.py
@@ -1,3 +1,5 @@
+###---------------------------------------------DAY 1
+
 # for i in range(1000):
 #     print(i+1)
 
@@ -74,11 +76,75 @@
 #         print(i)
 #     i+=1
 
-i=1
-while i<=100:
-    if i%2!=0:
-        print(i)
-    i+=1
+# i=1
+# while i<=100:
+#     if i%2!=0:
+#         print(i)
+#     i+=1
+
+
+
+###---------------------------------------------DAY 2
+
+# maxi=1000
+# total=0
+# for i in range(maxi+1):
+#     if i %2==0:
+#         total+=i
+# print(total)
+
+
+# maxi=1000
+# total=0
+# for i in range(maxi+1):
+#     if i %2!=0:
+#         total+=i
+# print(total)
+
+# for i in range(1,11):
+#     print(f"multiplication table of {i}")
+#     for j in range(1,11):
+#         print(f"{i}X{j}={i*j}")
+
+# user=int(input())
+# count=0
+# for i in range(1,user+1):
+#     count+=1
+
+# print(count)
+
+# fact=1
+# if user==0:
+#     fact=1
+# else:
+#     for i in range(1,user+1):
+#         fact*=i
+# print(fact)
+
+
+# num=int(input())
+# count=0
+# while num>0:
+#     count+=1
+#     num//=10
+# print(count)
+
+
+# num=int(input())
+# count=0
+# sum=0
+# while num>0:
+#     rem=num%10
+#     sum+=rem
+#     num//=10
+# print(sum)
+
+
+
+    
+
+
+
 
 
 
