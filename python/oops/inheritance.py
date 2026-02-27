c899a8a1f2f521120342256d8a766a9f0f887e70
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2026-02-27 11:44:24 +0530

    heirarical and multiple

--- python/oops/inheritance.py
+++ python/oops/inheritance.py
@@ -89,42 +89,136 @@
 # io.i13_details()
         
 
-class customer:
-    def __init__(self,name,mob):
-        self.name=name
-        self.mob=mob
-    def c_method(self):
-        print(self.name)
-        print(self.mob)
-class bank(customer):
-    def __init__(self, name, mob,balance,val_pin):
-        super().__init__(name, mob)
-        super().c_method()
-        self.balance=balance
-        self.val_pin=val_pin
-    def credit(self,amount,pin):
+# class customer:
+#     def __init__(self,name,mob):
+#         self.name=name
+#         self.mob=mob
+#     def c_method(self):
+#         print(self.name)
+#         print(self.mob)
+# class bank(customer):
+#     def __init__(self, name, mob,balance,val_pin):
+#         super().__init__(name, mob)
+#         super().c_method()
+#         self.balance=balance
+#         self.val_pin=val_pin
+#     def credit(self,amount,pin):
         
-        if pin==self.val_pin and amount>0:
-            self.balance+=amount
-            print("credit success")
-            print("Total balance")
-            print(self.balance)
-        else:
-            print("Invalid pin or amount")
-    def debit(self,amount,pin):
-        if pin==self.val_pin and amount>0 and amount<self.balance:
-            self.balance-=amount
-            print("debit success")
-            print("Total balance")
-            print(self.balance)
-        else:
-            print("Invalid pin or amount")
+#         if pin==self.val_pin and amount>0:
+#             self.balance+=amount
+#             print("credit success")
+#             print("Total balance")
+#             print(self.balance)
+#         else:
+#             print("Invalid pin or amount")
+#     def debit(self,amount,pin):
+#         if pin==self.val_pin and amount>0 and amount<self.balance:
+#             self.balance-=amount
+#             print("debit success")
+#             print("Total balance")
+#             print(self.balance)
+#         else:
+#             print("Invalid pin or amount")
 
-ob=bank("MK",7989020757,5000,1909)
-ob.credit(500,1909)
-ob.debit(50000,1909)
+# ob=bank("MK",7989020757,5000,1909)
+# ob.credit(500,1909)
+# ob.debit(50000,1909)
 
 
 
+class father:
+    def __init__(self,fname):
+        self.fname=fname
+    def f_details(self):
+        print(self.fname)
+class mother:
+    def __init__(self,mname):
+        self.mname=mname
+    def m_details(self):
+        print(self.mname)
+class child(father,mother):
+    def get_details(self,cname):
+        print(cname)
+        self.f_details()
+        self.m_details()
+        
+    
+
+
+
+
+# class animal:
+#     def __init__(self,bc):
+#         self.bc=bc
+#     def ani_det(self):
+#         print(self.bc)
+# class dog(animal):
+#     def __init__(self, bc,sound,ani):
+#         super().__init__(bc)
+#         self.sound=sound
+#         self.ani=ani
+#     def dog_det(self):
+#         super().ani_det()
+#         print(self.ani)
+#         print(self.sound)
+# class cat(animal):
+#     def __init__(self, bc,sound,ani):
+#         super().__init__(bc)
+#         self.sound=sound
+#         self.ani=ani
+#     def cat_det(self):
+#         super().ani_det()
+#         print(self.ani)
+#         print(self.sound)
+# class lion(animal):
+#     def __init__(self, bc,sound,ani):
+#         super().__init__(bc)
+#         self.sound=sound
+#         self.ani=ani
+#     def lion_det(self):
+#         super().ani_det()
+#         print(self.ani)
+#         print(self.sound)
+
+# obj=dog("red","bow","dog")
+# obj.dog_det()
+# obj2=cat("red","meow","cat")
+# obj2.cat_det()
+# obj3=lion("red","roar","lion")
+# obj3.lion_det()
+        
+        
+# class college:
+#     def __init__(self,cname):
+#         self.cname=cname
+#     def c_details(self):
+#         print(self.cname)
+# class cse(college):
+#     def __init__(self, cname,bname):
+#         super().__init__(cname)
+#         self.bname=bname
+#     def cse_details(self):
+#         super().c_details()
+#         print(self.bname)
+# class ece(college):
+#     def __init__(self, cname,bname):
+#         super().__init__(cname)
+#         self.bname=bname
+#     def ece_details(self):
+#         super().c_details()
+#         print(self.bname)
+# class mech(college):
+#     def __init__(self, cname,bname):
+#         super().__init__(cname)
+#         self.bname=bname
+#     def mech_details(self):
+#         super().c_details()
+#         print(self.bname)
+# obj1=cse("MRITS","CSE-B1")
+# obj1.cse_details()
+# obj2=ece("MRITS","ECE-B2")
+# obj2.ece_details()
+# obj3=mech("MRITS","MECH-B3")
+# obj3.mech_details()
 
         
