ff01bd7ab2337be963852fc789c3f9006e77300f
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2026-03-24 11:14:04 +0530

    merge

--- python/ps/listpro.py
+++ python/ps/listpro.py
@@ -1,27 +1,78 @@
-# add element to a list 
-list=[1,2,3]
-def add_element(num):
-    list.append(num)
-    return list
-a=add_element(4)
-print(a)
+# # add element to a list 
+# list=[1,2,3,2,5,2,7,2]
+# def add_element(num):
+#     list.append(num)
+#     return list
+# a=add_element(4)
+# print(a)
 
 
-# remove element in the list 
-def remove_element(num):
-    if num in list:
-        list.remove(num)
-    return list
-b=remove_element(4)
-print(b)
+# # remove element in the list 
+# def remove_element(num):
+#     if num in list:
+#         list.remove(num)
+#     return list
+# b=remove_element(4)
+# print(b)
 
 
-# find maximum number in a list
-maxim=list[0]
+# # find maximum number in a list
+# maxim=list[0]
+# for i in list:
+#     if i >maxim:
+#         maxim=i
+# print(maxim)
+
+
+# #count occurence of 
+# def count(value):
+#     count=0
+#     for i in list:
+#         if i==value:
+#             count+=1
+#     return count
+# num=2
+# d=count(num)
+# print(num,d)
+
+# # reverse a list
+# print(list[len(list)::-1])
+
+# # sorting a list
+# list=[1,6,2,8,7,3]
+# l=len(list)
+# for i in range(l):
+#     for j in range(i,l):
+#         if list[i]>list[j]:
+#             list[i],list[j]=list[j],list[i]
+# print(list)
+
+# remove duplicates
+list=[1,2,4,5,2,6,1,7,5]
+print(set(list))
+
+new_list=[]
+match=list[0]
 for i in list:
-    if i >maxim:
-        maxim=i
-print(maxim)
+    if i not in new_list:
+        new_list.append(i)
+print(new_list)
+
+
+# merging 2 lists
+l1=[1,2]
+l2=[3,4,2]
+l3=l1+l2 
+print(l3)
+
+# common elements in 2 list:
+for i in l1:
+    if i in l2:
+        print(i)
+
+
+
+
 
 
 
