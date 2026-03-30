29b0716352c444001cb8d4086e0b51758ba6eabb
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2026-03-30 10:50:58 +0530

    exception handling day1

---
+++ python/exception/exceptions.py
@@ -0,0 +1,50 @@
+
+
+
+# input=int(input("Enter a number:"))
+# try:
+#     res=input/0
+#     print(res)
+# except:
+#     print("zero division error")
+# # finally:
+# #     print("Execution completed")
+
+
+
+# set=int(input("Enter a number:"))
+# div=input("Enter a div:")
+# try:
+   
+#     res=set/int(div)
+#     print(res)
+# except ZeroDivisionError:
+#     print("Zero division error")
+# except ValueError:
+#     print("String is passes in div")
+
+# finally:
+#     print("Excution completed")
+
+
+# l1=[1,5,6]
+# num=3
+# try:
+#     print(l1[num])
+# except IndexError:
+#     print(f"the list has len of {len(l1)-1} but passed {num} as index value ")
+
+
+dici={"name":"Murthy","num":24}
+try:
+    print(dici["contact"])
+except KeyError:
+    print("Key not found")
+
+    
+
+
+
+
+
+
