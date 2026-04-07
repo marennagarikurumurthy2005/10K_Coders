ebb0b6a7eb2fe361250a32db311401f695d78dc3
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2026-04-07 12:52:43 +0530

    js conditional statements

---
+++ fronted/js/day3/conditions.js
@@ -0,0 +1,71 @@
+// let a=10
+// if (a>=0){
+//     console.log("positive number");
+    
+// }
+// else{
+//     console.log("negative number ")
+// }
+
+
+
+// const num=15
+// if (num%2==0){
+// console.log("even")
+// }
+// else{
+//     console.log("not")
+// }
+
+
+// const num=10
+// if (num==0){
+// console.log("zero value");
+
+// }
+// else{
+//     console.log("non zero");
+    
+// }
+
+
+// let age=25
+// if (age>=18){
+//     console.log("eligible");
+    
+// }
+
+// else{
+//     console.log("not eligible");
+    
+// }
+
+
+// const num1=10
+// const num2=20
+// if (num1>num2){
+//     console.log(num1);
+    
+// }
+// else{
+//     console.log(num2);
+    
+// }
+
+
+// let num=15
+// if (num%5==0){
+//     console.log("divisible by 5");
+    
+// }else{
+//     console.log("not divisible by 5");
+    
+// }
+
+
+let year=2026
+if ((year%4==0 && year%100!=0) || (year%400==0)){
+    console.log("leap year");
+    
+
+}
