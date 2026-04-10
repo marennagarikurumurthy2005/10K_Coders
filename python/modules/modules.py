b1f4817842d2a5acd94f82c5ca46799f5766d88a
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2026-04-10 11:14:19 +0530

    python modules

---
+++ python/modules/modules.py
@@ -0,0 +1,26 @@
+# user defined
+# from usermodule import add
+# print(add(5,10))
+
+
+# import math as m
+# print(m.sqrt(50))
+
+# print(m.ceil(5.3))
+# print(m.sin(45))
+# print(m.tan(45))
+
+# print(m.floor(510.9))
+
+# import random as r
+
+# print(r.randint(0,9))
+# print(r.random())
+
+
+
+
+
+
+
+
