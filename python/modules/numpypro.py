d62e89a70ca929740318c9125054ef6ac406e3ea
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2026-04-16 11:30:22 +0530

    maths

--- python/modules/numpypro.py
+++ python/modules/numpypro.py
@@ -28,12 +28,38 @@ import numpy as np
 
 ar1=np.array([1,2,3])
 ar2=np.array([4,5,6])
-print(ar1+ar2)
-print(ar1*ar2)
-print(ar2-ar1)
-print(ar2/ar1)
-print(ar1**ar2)
-print(ar2//ar1)
+# print(ar1+ar2)
+# print(ar1*ar2)
+# print(ar2-ar1)
+# print(ar2/ar1)
+# print(ar1**ar2)
+# print(ar2//ar1)
+
+
+# print(np.mean(ar1))
+# print(np.median(ar1))
+# # print(np.mod(ar1))
+# print(np.modf(ar1))
+# print(np.std(ar1))
+
+
+# s=np.sin(45)
+# c=np.cos(45)
+# print(s/c)
+
+
+# print(np.cos(45))
+# print(np.sin(45))
+# print(np.tan(45))
+
+# print(np.max(ar1))
+# print(np.min(ar1))
+
+# arr=np.array([[ar1],[ar2]])
+# print(np.sum(arr))
+
+
+
 
 
 
