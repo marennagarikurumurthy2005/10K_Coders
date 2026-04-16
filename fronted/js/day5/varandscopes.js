306f6befb557e666b8449ef8efdaf39ac231b5e4
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2026-04-16 12:41:52 +0530

    js var and scope of var

---
+++ fronted/js/day5/varandscopes.js
@@ -0,0 +1,52 @@
+//  types of variables let var  const
+
+
+// console.log(a)
+// let a=0
+// console.log(a);
+
+// console.log(b)
+// var b=1
+// console.log(b);
+
+// console.log(c)
+// const c=2
+// console.log(c);
+
+
+// let a
+// a=10
+// console.log(a);
+
+
+// let a=10
+// // a=20
+// // let a=20
+// console.log(a);
+
+
+// var a=10
+// // a=20
+// var a =20
+// console.log(a);
+
+
+
+const a=0
+// a=10 not possible
+
+console.log(a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
