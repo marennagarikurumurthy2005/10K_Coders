5292b098b78e2fae235547fd2b9dbb49b1c034a1
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2026-04-21 19:52:42 +0530

    nearest prime

--- DSA/nearestprime.py
+++ DSA/nearestprime.py
@@ -1,13 +1,30 @@
 
 
-# def primecheck(sup):
+def primecheck(sup):
+    for i in range(2,sup):
+        if sup%i==0:
+            return False
+    else:
+        return True
+
+n=1000
+lf=n-1
+rg=n+1
+c=0
+while True:
+    if primecheck(lf)==0:
+        lf-=1
+    else:
+        c+=1
+    if primecheck(rg)==0:
+        rg+=1
+    else:
+        c+=1
+    if c==2:
+        break
+print(lf,n,rg)
 
 
 
 
-
-
-# n=10
-# lf=n-1
-# rg=n+1
 
