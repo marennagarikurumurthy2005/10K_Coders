25f3d856ff59343eb19fb0e47a06b01bdf5e2831
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2026-04-22 19:41:37 +0530

    insertion sort

---
+++ DSA/insertionsort.py
@@ -0,0 +1,20 @@
+li=[0,2,4,8,-9,9,6,-1,-6]
+# for i in range(1,len(li)):
+#     key=li[i]
+#     index=i-1
+#     while key<li[index] and index>=0:
+#         li[i],li[index]=li[index],li[i]
+#         index-=1
+#         i-=1
+# print(li)
+
+
+for i in range(1,len(li)):
+    key=li[i]
+    index=i-1
+    while index>=0 and key<li[index]:
+        li[index+1]=li[index]
+        index-=1
+    li[index+1]=key
+print(li)        
+    
