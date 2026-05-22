b114a7fd7b63d9a717b50ed5a9905ff1d2525969
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2026-05-22 11:17:34 +0530

    voting

--- fronted/js/promises/scripts.js
+++ fronted/js/promises/scripts.js
@@ -1,19 +1,14 @@
 
 let display=document.getElementById('display')
-let agedata=document.getElementById("age")
 let btn=document.getElementById("btn")
-
 
 
 // age=agedata.textContent
 
 
 btn.addEventListener("click",()=>{
-    console.log(agedata)
-    age=agedata.innerText
-    console.log(age);
+    let age=document.getElementById("age").value
     
-
     let promise=new Promise((resolve, reject) => {
         
         if (age>18){
