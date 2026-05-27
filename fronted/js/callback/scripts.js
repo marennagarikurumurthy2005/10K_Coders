8734b39d7c38d568bd8fb956dfe253988f88781b
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2026-05-27 15:19:57 +0530

    promise chaining

--- fronted/js/callback/scripts.js
+++ fronted/js/callback/scripts.js
@@ -1,36 +1,105 @@
-function register(login){
-    setTimeout(() => {
-        console.log("registered");
-        login(dashboard)
-    }, 5000);
-}
+// function register(login){
+//     setTimeout(() => {
+//         console.log("registered");
+//         login(dashboard)
+//     }, 5000);
+// }
 
-function login(dashboard){
-    setTimeout(() => {
-        console.log("Login");
-        dashboard(payment)
-    }, 4000);
-}
+// function login(dashboard){
+//     setTimeout(() => {
+//         console.log("Login");
+//         dashboard(payment)
+//     }, 4000);
+// }
 
-function dashboard(payment){
-    setTimeout(() => {
-        console.log("dashboard");
-        payment(order)
-    }, 3000);
-}
+// function dashboard(payment){
+//     setTimeout(() => {
+//         console.log("dashboard");
+//         payment(order)
+//     }, 3000);
+// }
 
-function payment(order){
-    setTimeout(() => {
-        console.log("Payment done");
-        order()
-    }, 2000);
-}
+// function payment(order){
+//     setTimeout(() => {
+//         console.log("Payment done");
+//         order()
+//     }, 2000);
+// }
 
-function order(){
-    setTimeout(() => {
-        console.log("order placed");
+// function order(){
+//     setTimeout(() => {
+//         console.log("order placed");
         
-    }, 1000);
+//     }, 1000);
+// }
+
+// register(login)
+
+
+
+// using promie chaining
+
+function greenCart(str){
+    return new Promise((resolve, reject) => {
+        if (str){
+            resolve("successfully")
+        } else reject("Invalid credits")
+    })
+
 }
 
-register(login)
+user=greenCart(true)
+
+user
+    .then((res)=>{
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                console.log("registered",res);
+
+                resolve(res)
+                
+            }, 5000);
+            
+        })
+    })
+    .then((res)=>{
+
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                console.log("Login",res);
+
+                resolve(res)
+                
+            }, 4000);
+        })
+
+    })
+    .then((res)=>{
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                console.log("Dashboard");
+                resolve(res)
+                
+            }, 3000);
+        })
+    })
+    .then((res)=>{
+        return new Promise((resolve,reject)=>{
+            setTimeout(()=>{
+                    console.log("Payment completed",res)
+                    resolve(res)
+            },2000);
+        })
+    })
+    .then((res)=>{
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                console.log("orded placed",res);
+                
+            }, 1000);
+        })
+    })
+    .catch((rej)=>{
+        console.log(rej);
+        
+    })
