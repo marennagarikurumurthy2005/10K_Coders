a77787cbd736081c51833833c629819766f8fed3
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2026-06-05 12:52:51 +0530

    js strings

---
+++ fronted/js/strings/strings.js
@@ -0,0 +1,46 @@
+// creating a string
+//  string literals "" '' `${}` , string() , new string()
+
+// string methods
+
+// touppercase() , tolowercase() , at() , charat(),  chatcodeat(), concat(),  startwith() , endwith()
+// repeat() , replace() , replaceall() , slice(), substring(), length , trim() , trimstart(), trimend() 
+// padstart(), padend(), indexof(), lastindexof(), includes(), 
+let s="    hello world   "
+let s2="sitharamaraju"
+let s3="taxi-driver"
+let num="150"
+console.log(s.toUpperCase());
+console.log(s.toLowerCase());
+console.log(s.at(5));
+console.log(s.charAt(5));
+console.log(s.charCodeAt(5));
+console.log(s.concat(s2,s3));
+console.log(s3.startsWith("s"));
+console.log(s2.endsWith("s"));
+console.log(s3.repeat(10));
+console.log(s3.replace("x,'a"));
+console.log(s3.replaceAll("x,'a"));
+console.log(s2.slice(2,5));
+console.log(s2.substring(2,5));
+console.log(s2.length);
+console.log(s.includes("r"));
+console.log(s.indexOf('l'));
+console.log(s.lastIndexOf('l'));
+console.log(s.trim);
+console.log(s.trimEnd);
+console.log(s.trimStart);
+console.log(num.padEnd(10,0));
+console.log(num.padStart(10,0));
+
+
+
+
+
+
+
+
+
+
+
+
