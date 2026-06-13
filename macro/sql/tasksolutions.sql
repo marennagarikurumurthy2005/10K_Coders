d7d0845674b36022e1e1980bd396f380882b1d32
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2026-06-13 15:48:16 +0530

    macro sql task 2

--- macro/sql/tasksolutions.sql
+++ macro/sql/tasksolutions.sql
@@ -7,11 +7,31 @@ show TABLEs;
 
 CREATE table employee(emp_id INT PRIMARY KEY AUTO_INCREMENT,name VARCHAR(200),age INT,department VARCHAR(200),salary DECIMAL(10,2) NOT NULL);
 
-INSERT into employee(name,age,department,salary) VALUES
-('Ganesh',31,'sales',00000);
-
+INSERT into employee(name,age,salary) VALUES
+('Ganesh',31,00000);
 
 SELECT * from employee;
 
-
 DESC employee;
+
+-- Task 2: Table Modification
+-- Objective: Modify an existing table using DDL commands.
+-- Requirements:
+-- ● Add email column with UNIQUE constraint
+-- ● Add default value to department
+-- ● Drop an unused column
+
+alter Table employee
+add COLUMN email VARCHAR(50) UNIQUE;
+
+ALTER Table employee
+MODIFY  department VARCHAR(200) DEFAULT 'ninja';
+
+UPDATE employee
+SET email="murthy@gmail.com" where emp_id=1;
+
+DELETE FROM employee
+WHERE emp_id=8 or emp_id=7;
+
+ALTER Table employee
+DROP COLUMN age;
