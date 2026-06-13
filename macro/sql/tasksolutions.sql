4ba20d176ac3fd6f104f3d8edb5e5b1ae9fb2ca5
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2026-06-13 19:35:38 +0530

    task3 macro sql

--- macro/sql/tasksolutions.sql
+++ macro/sql/tasksolutions.sql
@@ -35,3 +35,40 @@ WHERE emp_id=8 or emp_id=7;
 
 ALTER Table employee
 DROP COLUMN age;
+
+-- ask 3: Employee Data Management
+-- Objective: Perform data manipulation operations.
+-- Requirements:
+-- ● Insert at least 10 employee records
+-- ● Update salary of a department
+-- ● Delete employees with NULL department
+
+drop Table employee;
+
+CREATE table employee(emp_id INT PRIMARY KEY AUTO_INCREMENT,name VARCHAR(200),age INT,department VARCHAR(200),salary DECIMAL(10,2) NOT NULL);
+
+INSERT into employee(name,age,department,salary)
+VALUES
+("kurumurthy",21,'SWE',70000),
+("Devratha",51,'Sales',60000),
+("Vardha",51,'admini',62000),
+("Pandith",29,'SWE',70000),
+("Bilal",41,'support',40000);
+
+INSERT INTO employee(name,age,salary)
+VALUES
+('dileep',40,45624),
+('sandeep',30,45624),
+('sunil',41,45624),
+('vishal',43,45624),
+('sreenu',30,45624);
+
+SELECT * from employee;
+
+UPDATE employee
+set salary=85000 WHERE department="swe";
+
+DELETE  FROM employee
+WHERE department is NULL;
+
+
