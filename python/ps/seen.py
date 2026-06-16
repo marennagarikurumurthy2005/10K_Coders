dec60e63926ae244bb423001022c976476151a99
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2026-06-16 17:17:17 +0530

    words to numbers

--- python/ps/seen.py
+++ python/ps/seen.py
@@ -26,21 +26,41 @@
 
 # sorting colors
 
-colors=[1,0,2,1,0,2]
-low=mid=0
-high=len(colors)-1
+# colors=[1,0,2,1,0,2]
+# low=mid=0
+# high=len(colors)-1
 
-while mid<=high:
-    if colors[mid]==0:
-        colors[mid],colors[low]=colors[low],colors[mid]
-        low+=1
-        mid+=1
-    elif colors[mid]==1:
-        mid+=1
-    else:
-        colors[high],colors[mid]=colors[mid],colors[high]
-        high-=1
-print(colors)
+# while mid<=high:
+#     if colors[mid]==0:
+#         colors[mid],colors[low]=colors[low],colors[mid]
+#         low+=1
+#         mid+=1
+#     elif colors[mid]==1:
+#         mid+=1
+#     else:
+#         colors[high],colors[mid]=colors[mid],colors[high]
+#         high-=1
+# print(colors)
 
 
-# s="fourtwosevensix"
+s="fourtwosevensix"
+dici={
+    "one":'1',
+    "two":'2',
+    "three":'3',
+    "four":'4',
+    "five":'5',
+    "six":'6',
+    "seven":'7',
+    "eight":'8',
+    "nine":'9'
+}
+sub=""
+num=""
+for i in s:
+    sub+=i
+    if sub in dici:
+        num=num+dici[sub]
+        sub=""
+print(num)
+    
