a2c013efce0104ddfd4943391a33156cb1e94087
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2026-07-09 17:10:16 +0530

    email fix

--- session/app1/views.py
+++ session/app1/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import json
 from django.views.decorators.csrf import csrf_exempt
-from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives, send_mail
 from django.conf import settings
 
 mail = settings.EMAIL_HOST_USER
@@ -26,11 +26,30 @@ mail = settings.EMAIL_HOST_USER
 #     return JsonResponse({'status':'cant find session'})
 
 def welcome(req):
-    send_mail(
-    subject = "Test Email from Django",
-    message = "Hello, this email is sent using Django.",
-    from_email = 'marennagarikurumurthy2005@gmail.com@gmail.com',
-    recipient_list = ["kurumurthy724@gmail.com"],
-    fail_silently=True
+    # send_mail(
+    # subject = "Test Email from Django",
+    # message = "Hello, this email is sent using Django.",
+    # from_email = 'marennagarikurumurthy2005@gmail.com@gmail.com',
+    # recipient_list = ["kurumurthy724@gmail.com"],
+    # fail_silently=True
+    # ) nayabrasool2050@gmail.com
+    # return JsonResponse({'status':"Email Sent Successfully"})
+    subject = "HTML Email Test"
+    from_email = "marennagarikurumurthy2005@gmail.com"
+    recipient_list = ["kurumurthy724@gmail.com","nayabrasool2050@gmail.com"]
+    fail_silently=False
+    text_content = "This is a test email."
+    html_content = """
+    <h1 style="color:blue;">Welcome</h1>
+    <p>Hello, this email is sent using <b>Django</b>.</p>
+    """
+    email = EmailMultiAlternatives(
+    subject,
+    text_content,
+    from_email,
+    recipient_list,
+    fail_silently
     )
-    return JsonResponse({'status':"Email Sent Successfully"})
+    email.attach_alternative(html_content, "text/html")
+    email.send()
+    return JsonResponse({"status":"HTML Email Sent"})
