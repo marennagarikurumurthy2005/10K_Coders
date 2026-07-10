a27aac64c9f3b2c4caf118a2ff5b0c433049005e
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2026-07-10 14:06:59 +0530

    class based views

--- cloudinary/app1/models.py
+++ cloudinary/app1/models.py
@@ -5,3 +5,4 @@ from django.db import models
 class Surya_table(models.Model):
     name=models.CharField(max_length=100)
     age=models.IntegerField()
+    image=models.URLField()
--- cloudinary/app1/views.py
+++ cloudinary/app1/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+
+@method_decorator(csrf_exempt,name='dispatch')
+class CreateView(View):
+    def get(self,req):
+        return JsonResponse({'status':"this is for get method"})
+    def post(self,req):
+        return JsonResponse({'status':"this is for post method"})
+    def delete(self,req):
+        return JsonResponse({'status':"this is for delete method"})
+
