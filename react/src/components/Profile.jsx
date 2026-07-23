bae4920bc225a7d43058020d7b37454917117b44
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2026-07-23 12:57:34 +0530

    react 1st

---
+++ react/src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div>
+        <h1>Murthy</h1>
+        <img src="https://static.vecteezy.com/system/resources/thumbnails/032/176/197/small/business-avatar-profile-black-icon-man-of-user-symbol-in-trendy-flat-style-isolated-on-male-profile-people-diverse-face-for-social-network-or-web-vector.jpg" alt="" />
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus voluptas quas facilis, possimus numquam mollitia cumque sint blanditiis aperiam, corporis explicabo hic molestias iusto ipsam aspernatur dolorum fugiat ad placeat?</p>
+    </div>
+  )
+}
+
+export default Profile
