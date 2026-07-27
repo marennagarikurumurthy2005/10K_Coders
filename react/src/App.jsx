64625ec82b6d843b4c5ccb4d74dce552767e9b95
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2026-07-27 15:18:29 +0530

    passing throught props

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,10 +1,55 @@
 import React from 'react'
 import Profile from './components/Profile'
 
+const users = [
+  {
+    id: 1,
+    name: "Kurumurthy",
+    role: "Python Full Stack Developer",
+    email: "kurumurthy@gmail.com",
+    age: 22,
+    isOnline: true,
+    skills: ["Python", "Django", "React", "SQL"],
+    address: {
+      city: "Hyderabad",
+      state: "Telangana",
+      country: "India"
+    }
+  },
+  {
+    id: 2,
+    name: "",
+    role: "Frontend Developer",
+    email: "@gmail.com",
+    age: 21,
+    isOnline: false,
+    skills: ["React", "JavaScript", "Tailwind"],
+    address: {
+      city: "Bangalore",
+      state: "Karnataka",
+      country: "India"
+    }
+  },
+  {
+    id: 3,
+    name: "Jagadish",
+    role: "Backend Developer",
+    email: "jagadish@gmail.com",
+    age: 23,
+    isOnline: true,
+    skills: ["Python", "FastAPI", "PostgreSQL"],
+    address: {
+      city: "Chennai",
+      state: "Tamil Nadu",
+      country: "India"
+    }
+  }
+];
+
 const App = () => {
   return (
     <div>
-      <Profile/>
+      <Profile users={users}/>
     </div>
   )
 }
--- react/src/components/Profile.jsx
+++ react/src/components/Profile.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import User from './User'
 
-const Profile = () => {
+const Profile = ({users}) => {
   return (
     <div>
-        <h1>Murthy</h1>
+
+        {users.map((user)=>{
+          return <User key={user.id} user={user}/>
+        })}
+        {/* <h1>Murthy</h1>
         <img src="https://static.vecteezy.com/system/resources/thumbnails/032/176/197/small/business-avatar-profile-black-icon-man-of-user-symbol-in-trendy-flat-style-isolated-on-male-profile-people-diverse-face-for-social-network-or-web-vector.jpg" alt="" />
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus voluptas quas facilis, possimus numquam mollitia cumque sint blanditiis aperiam, corporis explicabo hic molestias iusto ipsam aspernatur dolorum fugiat ad placeat?</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus voluptas quas facilis, possimus numquam mollitia cumque sint blanditiis aperiam, corporis explicabo hic molestias iusto ipsam aspernatur dolorum fugiat ad placeat?</p> */}
     </div>
   )
 }
